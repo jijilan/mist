853817ac5dd9eab224f9283e90b9c00f2cca6e8a
JiJl <jijilan>
2019-10-27 14:59:13 +0800

    自定义异常类

---
+++ src/main/java/cn/jijl/mist/common/exception/MyException.java
@@ -0,0 +1,25 @@
+package cn.jijl.mist.common.exception;
+
+import cn.jijl.mist.common.result.ResultEnum;
+import lombok.Getter;
+
+/**
+ * @Author: liujiebang
+ * @Description: 自定义异常类
+ * @Date: 2018/7/2 16:48
+ **/
+@Getter
+public class MyException extends RuntimeException {
+
+    private ResultEnum resultEnum;
+
+    public MyException(ResultEnum resultEnum) {
+        super(resultEnum.getMsg());
+        this.resultEnum = resultEnum;
+    }
+
+    public MyException(ResultEnum resultEnum, String message) {
+        super(resultEnum.getMsg() + message);
+        this.resultEnum = resultEnum;
+    }
+}
